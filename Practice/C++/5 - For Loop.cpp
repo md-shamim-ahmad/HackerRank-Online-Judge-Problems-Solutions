2d7b0221df20b82a96884ed3fb7632ebb53d0e8a
Shamim-Nik <56692841+Shamim-Nik@users.noreply.github.com>
2020-01-25 05:01:55 +0600

    Update 5 - For Loop.cpp

--- Practice/C++/5 - For Loop.cpp
+++ Practice/C++/5 - For Loop.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
+char ch[10][10] = {"one","two","three","four","five","six","seven","eight","nine"};
 
 int main() {
     // Complete the code.
@@ -9,35 +10,7 @@ int main() {
     for(int i=n;i<=m;i++){
         if(i<=9)
         {
-            switch(i){
-              case 1:
-                cout<<"one"<<endl;
-                break;
-              case 2:
-                cout<<"two"<<endl;
-                break;
-              case 3:
-                cout<<"three"<<endl;
-                break;
-              case 4:
-                cout<<"four"<<endl;
-                break;
-              case 5:
-                cout<<"five"<<endl;
-                break;
-              case 6:
-                cout<<"six"<<endl;
-                break;
-              case 7:
-                cout<<"seven"<<endl;
-               break;
-              case 8:
-                cout<<"eight"<<endl;
-                break;
-              case 9:
-                cout<<"nine"<<endl;
-                break;
-            }
+            cout << ch[i - 1] << endl;
         }
         else if ( i>9) {
             if(i%2==0)
