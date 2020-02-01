b124816aca1a819c774f614a31d0be7dccda3b67
Shamim-Nik <56692841+Shamim-Nik@users.noreply.github.com>
2020-02-02 03:47:53 +0600

    Update 6 - Functions.cpp

--- Practice/C++/6 - Functions.cpp
+++ Practice/C++/6 - Functions.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
 using namespace std;
-int max_of_four(const int& a,const int &b,const int &c,const int &d)
-{
-    int l=max(a, b);
-    int m=max(c, d);
-    return (max(l,m));
+
+int max_of_four(int a, int b, int c, int d) {
+    return max(a,max(b,(max(c,d))));
 }
+
 int main() {
     int a, b, c, d;
     scanf("%d %d %d %d", &a, &b, &c, &d);
     int ans = max_of_four(a, b, c, d);
     printf("%d", ans);
+    
     return 0;
 }
