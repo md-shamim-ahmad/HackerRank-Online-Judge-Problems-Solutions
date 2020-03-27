d8095181f043c9a54881129e05018ce7e92497d2
Shamim-Nik <56692841+Shamim-Nik@users.noreply.github.com>
2020-03-28 01:22:32 +0600

    Update Overload Operators.cpp

--- Practice/C++/Overload Operators.cpp
+++ Practice/C++/Overload Operators.cpp
@@ -1,13 +1,63 @@
+//Operator Overloading
 
+#include<iostream>
 
-Complex operator +(const Complex &a,const Complex &b){
-    Complex output;
-    output.a=a.a+b.a;
-    output.b=a.b+b.b;
-    return output;
-}
-namespace std{
-    ostream& operator<<(ostream& out,const Complex &output){
-        return out<<output.a<<"+i"<<output.b;
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+        cout << a << " " << b << endl;
     }
+};
+
+Complex operator+ (Complex const& obj1, Complex const& obj2) {
+    Complex ob;
+    ob.a = obj1.a + obj2.a;
+    ob.b = obj1.b + obj2.b;
+    return ob;
+}
+
+ostream& operator<< (ostream& out, Complex const& obj) { 
+    out << obj.a;
+    out << "+i" << obj.b << endl; 
+    return out; 
+} 
+
+//Overload operators + and << for the class complex
+//+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
+//<< should print a complex number in the format "a+ib"
+
+int main()
+{
+    Complex x,y;
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    x.input(s1);
+    y.input(s2);
+    Complex z=x+y;
+    cout<<z<<endl;
 }
