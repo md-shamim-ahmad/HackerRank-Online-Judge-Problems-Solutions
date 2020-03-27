a1c03eda1a809d577cb0a66bcd8a948acec88215
Shamim-Nik <56692841+Shamim-Nik@users.noreply.github.com>
2020-03-27 20:12:11 +0600

    Create Day 16: Exceptions - String to Integer.java

---
+++ Practice/Tutorials/30 Days of Code/Day 16: Exceptions - String to Integer.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        
+        try {
+            int num = Integer.parseInt(S);
+            System.out.println(num);
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+    }
+}
