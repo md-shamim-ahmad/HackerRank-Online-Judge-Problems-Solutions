e3398665355863fd532f41b488b3ee90bd7b7d5c
Shamim-Nik <56692841+Shamim-Nik@users.noreply.github.com>
2020-03-30 13:41:18 +0600

    Create List Comprehensions.py

---
+++ Practice/Python/List Comprehensions.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    ar = []
+    p = 0
+    for i in range(x + 1):
+        for j in range(y + 1):
+            for k in range(z + 1):
+                if i + j + k != n:
+                    ar.append([])
+                    ar[p] = [i, j, k]
+                    p += 1
+    print(ar)
