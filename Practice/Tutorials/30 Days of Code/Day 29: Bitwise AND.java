28af6ca7d92a8a873401a95dbf5da04ce11b8f63
Shamim-Nik <shamimahmmed9138@gmail.com>
2020-04-24 14:10:17 +0600

    Create Day 29: Bitwise AND.java

---
+++ Practice/Tutorials/30 Days of Code/Day 29: Bitwise AND.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+            int k = Integer.parseInt(nk[1]);
+            if (((k - 1) | k) > n && k % 2 == 0) {
+                System.out.println(k - 2);
+            } else {
+                System.out.println(k - 1);
+            }
+        }
+        scanner.close();
+    }
+}
