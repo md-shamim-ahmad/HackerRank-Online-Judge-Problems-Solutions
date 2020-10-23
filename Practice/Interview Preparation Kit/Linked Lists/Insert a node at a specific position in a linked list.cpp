e48409445526bdf545636ab3b763ef0ed6e9dc29
Md Shamim Ahmmed <56692841+Md-Shamim-Ahmmed@users.noreply.github.com>
2020-10-23 09:16:26 +0600

    Create Insert a node at a specific position in a linked list.cpp

---
+++ Practice/Interview Preparation Kit/Linked Lists/Insert a node at a specific position in a linked list.cpp
@@ -0,0 +1,28 @@
+
+
+// Complete the insertNodeAtPosition function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+    SinglyLinkedListNode *temp = new SinglyLinkedListNode(data);
+    SinglyLinkedListNode *current = head;
+    if (head == nullptr) {
+        temp->next = head;
+        head = temp;
+        return head;
+    } else {
+        for (int i = 0; i < position - 1; i++)
+            current = current->next;
+        temp->next = current->next;
+        current->next = temp;
+        return head;
+    }
+}
