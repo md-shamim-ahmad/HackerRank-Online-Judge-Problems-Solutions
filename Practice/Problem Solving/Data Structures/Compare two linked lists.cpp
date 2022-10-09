c0b556c17e87be51e33017de5971acdc8c5ec675
Md-Shamim-Ahmmed <shamim.pust.cse11@gmail.com>
2022-10-09 16:16:08 +0600

    Compare two linked lists

---
+++ Practice/Problem Solving/Data Structures/Compare two linked lists.cpp
@@ -0,0 +1,16 @@
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    while(head1 != nullptr && head2 != nullptr) {
+        if ((head1 != nullptr && head2 == nullptr) || (head1 == nullptr && head2 != nullptr)) {
+            return false;
+        }
+        if (head1 -> data != head2 -> data) {
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    if ((head1 != nullptr && head2 == nullptr) || (head1 == nullptr && head2 != nullptr)) {
+        return false;
+    }
+    return true;
+}
